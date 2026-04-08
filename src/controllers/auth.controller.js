2b434d578b17349e8f5d890c77aa071ca5c66a30
Syed Muhammad Anas <anas.syed19c1@gmail.com>
2026-04-08 11:10:59 +0500

    feat: login controller added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,7 +47,62 @@ export const register = async (req, res) => {
     });
 
   } catch (error) {
-    console.log(error)
+    return sendResponse(res, 500, "Server error", error.message);
+  }
+};
+
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check Fields
+    if (!email || !password) {
+      return sendResponse(res, 400, "Email aur password dono chahiye");
+    }
+
+    // User dhundho
+    const user = await User.findOne({ email });
+    if (!user) {
+      return sendResponse(res, 404, "Invalid Username & Password");
+    }
+
+    // Password check karo
+    const isPasswordValid = await user.isPasswordCorrect(password);
+    if (!isPasswordValid) {
+      return sendResponse(res, 401, "Invalid Username & Password");
+    }
+
+    // Tokens banao
+    const accessToken = generateAccessToken(user._id, user.role);
+    const refreshToken = generateRefreshToken(user._id);
+
+    // Refresh Token DB mein save karo
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    // Refresh Token cookie mein rakho
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: ENV.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    // Response bhejo
+    return sendResponse(res, 200, "Login successful", {
+      accessToken,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+    console.log(error);
     return sendResponse(res, 500, "Server error", error.message);
   }
 };
