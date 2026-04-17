85574a019461b9b795515c8d6b793bef36164fe7
Syed Muhammad Anas <anas.syed19c1@gmail.com>
2026-04-18 00:52:46 +0500

    fix: add PORT for railways

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import app from './src/app.js'
 import connectDB from './src/config/db.js';
 import { ENV } from './src/config/env.js';
 
-
 connectDB()
 
+const PORT = process.env.PORT || ENV.PORT || 5000;
 
-
-app.listen(ENV.PORT, () => {
-    console.log(`server running on PORT ${ENV.PORT}`);
+app.listen(PORT, () => {
+    console.log(`server running on PORT ${PORT}`);
 })
